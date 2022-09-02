ab7e48fc8eb7d08f691f280737a0e6d41920b5e4
PatricVisdom <patricvisdom@govisdom.com>
2022-09-02 12:05:38 +0200

    use prettier form ts formatting

--- neovim/.config/nvim/lua/user/null-ls.lua
+++ neovim/.config/nvim/lua/user/null-ls.lua
@@ -3,27 +3,27 @@
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 require("null-ls").setup({
-  sources = {
-    require("null-ls").builtins.formatting.eslint.with({
-      filetypes = { "typescript" }
-    }),
-    require("null-ls").builtins.formatting.stylua.with({
-      filetypes = { "lua" }
-    }),
-    require("null-ls").builtins.code_actions.gitsigns,
-  },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-          --vim.lsp.buf.formatting_sync()
-          vim.lsp.buf.format({ bufnr = bufnr })
-        end,
-      })
-    end
-  end,
+	sources = {
+		require("null-ls").builtins.formatting.prettier.with({
+			filetypes = { "typescript" },
+		}),
+		require("null-ls").builtins.formatting.stylua.with({
+			filetypes = { "lua" },
+		}),
+		require("null-ls").builtins.code_actions.gitsigns,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					--vim.lsp.buf.formatting_sync()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
