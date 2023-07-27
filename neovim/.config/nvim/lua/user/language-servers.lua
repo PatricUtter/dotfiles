98fe494ac93c7790e614a0cfc2344793cdcf74b0
PatricVisdom <patricvisdom@govisdom.com>
2023-07-27 09:13:43 +0200

    write things correctly etc

--- neovim/.config/nvim/lua/user/language-servers.lua
+++ neovim/.config/nvim/lua/user/language-servers.lua
@@ -8,17 +8,13 @@ local function global_on_attach(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gi", "<cmd> vim.lsp.buf.implementation()<CR>", opts)
-  --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  --vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  --vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gf", "<cmd> lua vim.lsp.buf.references()<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gf", "<cmd>Telescope lsp_references<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>gf",
+    "<cmd>lua require('telescope.builtin').lsp_references({show_line = false})<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.lsp.buf.format({ async: true })<CR>", opts)
-  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>e", "<cmd>lua vim.diagnostic.open_float()<C>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]g", "<cmd>lua vim.diagnostic.goto_next({ float: true })<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "[g", "<cmd>lua vim.diagnostic.open_float({ float: true })<CR>", opts)
 end
@@ -56,7 +52,7 @@ require("mason-lspconfig").setup_handlers({
       settings = {
         Lua = {
           diagnostics = {
-            globals = { "vim", "use" },
+            globals = { 'vim', 'use' },
           },
         },
       },
--- neovim/.config/nvim/lua/user/telescope.lua
+++ neovim/.config/nvim/lua/user/telescope.lua
@@ -2,25 +2,25 @@
 require("telescope").load_extension("fzf")
 
 require("telescope").setup({
-	defaults = {
-		mappings = {
-			i = {
-				["<C-k>"] = require("telescope.actions").move_selection_previous,
-				["<C-j>"] = require("telescope.actions").move_selection_next,
-				["<C-q>"] = require("telescope.actions").send_selected_to_qflist,
-			},
-		},
-		display_path = { "tail" },
-	},
-	extensions = {
-		fzf = {
-			fuzzy = true, -- false will only do exact matching
-			override_generic_sorter = true, -- override the generic sorter
-			override_file_sorter = true, -- override the file sorter
-			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			-- the default case_mode is "smart_case"
-		},
-	},
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-k>"] = require("telescope.actions").move_selection_previous,
+        ["<C-j>"] = require("telescope.actions").move_selection_next,
+        ["<C-q>"] = require("telescope.actions").send_selected_to_qflist,
+      },
+    },
+    path_display = { "truncate" },
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                   -- false will only do exact matching
+      override_generic_sorter = true, -- override the generic sorter
+      override_file_sorter = true,    -- override the file sorter
+      case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    },
+  },
 })
 
 require("telescope").load_extension("ui-select")
